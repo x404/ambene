14bdedc62fba49ecea52b8e521247dc36dd3c713
x404 <kronos2003@gmail.com>
2017-09-17 18:52:25 +0300

    modal вынесен в libs.js

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,9 @@ gulp.task('browser-sync', function(){
 gulp.task('compress', function(){
 	pump([
 			gulp.src([  // Берем все необходимые библиотеки
-				config.libsDir + '/jquery/dist/jquery.js'
+				config.libsDir + '/jquery/dist/jquery.js',
+				config.sourceDir + '/js/util.js',
+				config.sourceDir + '/js/modal.js'
 			]),
 			concat('libs.min.js'), // Собираем их в кучу в новом файле libs.min.js
 			uglify(), // Сжимаем JS файл
